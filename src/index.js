691b2a768ff394db948d7b108dc936d1c7432e15
Skladaniuk <skladanyuk1999@gmail.com>
2022-06-13 02:47:13 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
-import './css/styles.css';
-
-const DEBOUNCE_DELAY = 300;
+import './css/styles.css'; 
+import FETCH from './fetchCountries.js' 
+import debounce from 'lodash.debounce' 
+ 
+const DEBOUNCE_DELAY = 300; 
+ 
+ 
+ const refs = { 
+    input:document.querySelector('#search-box'), 
+    countryList:document.querySelector('.country-list'), 
+    countryInfo:document.querySelector('.country-info'), 
+ }   
+    
+ 
+ 
+  
+// const arrayCountry = refs.input.addEventListener('input', debounce(FETCH,DEBOUNCE_DELAY)); 
+   
+ 
+ console.log(arrayCountry); 
+ 
+ 
+ function countryListMarkup(arrayCountry) { 
+    refs.countryList.innerHTML = arrayCountry 
+     .map(({ name, flags }) => { 
+       return `<li class="country-list__item"><img src="${flags.svg}" alt="${name.official}" class="country-list_img"><span class="country-list__name">${name.official}</span></li>; 
+     `}) 
+     .join(''); 
+      
+   }
